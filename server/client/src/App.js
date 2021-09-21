3d602b2736abd5f9f3d2b2f87340d39c467c031c
scottwarburton <scott_warburton@hotmail.com>
2021-09-21 12:59:54 +1000

    Debug

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Selection from "./components/Selection"
+import { BrowserRouter as Router} from "react-router-dom"
+import React from "react"
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Learning Mandarin</h1>
-      <h3>Welcome to Learning Mandarin by Scott</h3>
-      <Selection />
-    </div>
+    <Router>
+      <div className="container">
+        <h1>Learning Mandarin</h1>
+        <h3>Welcome to Learning Mandarin by Scott</h3>
+        <br/><br/>
+        <Selection />
+      </div>
+    </Router>
   );
 }
 
