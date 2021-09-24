73a4f82f9253a369e99314d4f0f757f08fc1a9f3
scottwarburton <scott_warburton@hotmail.com>
2021-09-24 16:43:19 +1000

    Add lessons

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -6,7 +6,10 @@ import Lesson from "./components/Lesson"
 function App() {
   const [topic, setTopic] = useState("");
   const [lesson, setLesson] = useState("");
-  const [flashcards, setFlashcards] = useState(FLASHCARDS);
+  const [greetings, setGreetings] = useState(GREETINGS);
+  const [introduction, setIntroduction] = useState(INTRODUCTION);
+  const [transport, setTransport] = useState(TRANSPORT);
+  const [locations, setLocations] = useState(LOCATIONS);
 
   return (
     <div id="app">
@@ -46,10 +49,10 @@ function App() {
       }
 
       <div id="box">
-        {lesson === "Greetings" ? <Lesson topic="Greetings" flashcards={flashcards} /> : null}
-        {lesson === "Introduction" ? <Lesson topic="Introduction" flashcards={flashcards} /> : null}
-        {lesson === "Transport" ? <Lesson topic="Transport" flashcards={flashcards} /> : null}
-        {lesson === "Locations" ? <Lesson topic="Locations" flashcards={flashcards} /> : null}
+        {lesson === "Greetings" ? <Lesson topic="Greetings" flashcards={greetings} /> : null}
+        {lesson === "Introduction" ? <Lesson topic="Introduction" flashcards={introduction} /> : null}
+        {lesson === "Transport" ? <Lesson topic="Transport" flashcards={transport} /> : null}
+        {lesson === "Locations" ? <Lesson topic="Locations" flashcards={locations} /> : null}
       </div>
     </div>
   );
@@ -59,131 +62,443 @@ function App() {
 export default App;
 
 
-const FLASHCARDS = [
+const GREETINGS = [
   {
       id: 1,
       english: "hello",
       mandarin: "你好",
-      pinyin: "nǐ hǎo",
-      sentence: "sentence1"
+      pinyin: "nǐ hǎo"
   },
   {
       id: 2,
       english: "goodbye",
       mandarin: "再见",
-      pinyin: "zàijiàn",
-      sentence: "sentence2"
+      pinyin: "zàijiàn"
   },
   {
       id: 3,
       english: "he/her",
       mandarin: "他/她",
-      pinyin: "tā",
-      sentence: "sentence3"
+      pinyin: "tā"
   },
   {
       id: 4,
       english: "we",
       mandarin: "我们",
-      pinyin: "wǒmen",
-      sentence: ""
+      pinyin: "wǒmen"
   },
   {
       id: 5,
       english: "thank you",
       mandarin: "谢谢",
-      pinyin: "xièxiè",
-      sentence: ""
+      pinyin: "xièxiè"
   },
   {
       id: 6,
       english: "name",
       mandarin: "名字",
-      pinyin: "míngzì",
-      sentence: ""
+      pinyin: "míngzì"
   },
   {
       id: 7,
       english: "yes",
       mandarin: "是的",
-      pinyin: "shì de",
-      sentence: ""
+      pinyin: "shì de"
   },
   {
       id: 8,
       english: "no",
       mandarin: "不是",
-      pinyin: "bùshì",
-      sentence: ""
+      pinyin: "bùshì"
   },
   {
       id: 9,
       english: "excuse me",
       mandarin: "不好意思",
-      pinyin: "bù hǎoyìsi",
-      sentence: ""
+      pinyin: "bù hǎoyìsi"
   },
   {
       id: 10,
       english: "you're welcome",
       mandarin: "不客气",
-      pinyin: "bù kèqì",
-      sentence: ""
+      pinyin: "bù kèqì"
   },
   {
       id: 11,
       english: "no problem",
       mandarin: "没关系",
-      pinyin: "méiguānxì",
-      sentence: ""
+      pinyin: "méiguānxì"
   },
   {
       id: 12,
       english: "recently",
       mandarin: "最近",
-      pinyin: "zuìjìn",
-      sentence: ""
+      pinyin: "zuìjìn"
   },
   {
       id: 13,
       english: "good",
       mandarin: "好",
-      pinyin: "hǎo",
-      sentence: ""
+      pinyin: "hǎo"
   },
   {
       id: 14,
       english: "not good",
       mandarin: "不好",
-      pinyin: "bù hǎo",
-      sentence: ""
+      pinyin: "bù hǎo"
   },
   {
       id: 15,
       english: "very",
       mandarin: "很",
-      pinyin: "hěn",
-      sentence: ""
+      pinyin: "hěn"
   },
   {
       id: 16,
       english: "and you?",
       mandarin: "你呢",
-      pinyin: "nǐ ne",
-      sentence: ""
+      pinyin: "nǐ ne"
   },
   {
       id: 17,
       english: "called",
       mandarin: "叫",
-      pinyin: "jiào",
-      sentence: ""
+      pinyin: "jiào"
   },
   {
       id: 18,
       english: "how is",
       mandarin: "怎么样",
-      pinyin: "zěnme yàng",
-      sentence: ""
+      pinyin: "zěnme yàng"
+  }
+]
+const INTRODUCTION = [
+  {
+      id: 1,
+      english: "",
+      mandarin: "",
+      pinyin: ""
+  },
+  {
+    id: 2,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 3,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 4,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 5,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 6,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 7,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 8,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 9,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 10,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 11,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 12,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 13,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 14,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 15,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 16,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 17,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  },
+  {
+    id: 18,
+    english: "",
+    mandarin: "",
+    pinyin: ""
+  }
+]
+const TRANSPORT = [
+  {
+    id: 1,
+    english: "car",
+    mandarin: "车",
+    pinyin: "chē"
+  },
+  {
+    id: 2,
+    english: "train",
+    mandarin: "火车",
+    pinyin: "huǒchē"
+  },
+  {
+    id: 3,
+    english: "plane",
+    mandarin: "飞机",
+    pinyin: "fēijī"
+  },
+  {
+    id: 4,
+    english: "motorcycle",
+    mandarin: "摩托车",
+    pinyin: "mótuō chē"
+  },
+  {
+    id: 5,
+    english: "scooter",
+    mandarin: "电动车",
+    pinyin: "diàndòng chē"
+  },
+  {
+    id: 6,
+    english: "bicycle",
+    mandarin: "自行车",
+    pinyin: "zìxíngchē"
+  },
+  {
+    id: 7,
+    english: "bus",
+    mandarin: "公车",
+    pinyin: "gōngchē"
+  },
+  {
+    id: 8,
+    english: "train station",
+    mandarin: "火车站",
+    pinyin: "zhàn"
+  },
+  {
+    id: 9,
+    english: "airport",
+    mandarin: "飞机场",
+    pinyin: "fēijī chǎng"
+  },
+  {
+    id: 10,
+    english: "ticket",
+    mandarin: "票",
+    pinyin: "piào"
+  },
+  {
+    id: 11,
+    english: "subway",
+    mandarin: "地铁",
+    pinyin: "dìtiě"
+  },
+  {
+    id: 12,
+    english: "taxi",
+    mandarin: "出租车",
+    pinyin: "chūzū chē"
+  },
+  {
+    id: 13,
+    english: "to take",
+    mandarin: "乘坐",
+    pinyin: "chéngzuò"
+  },
+  {
+    id: 14,
+    english: "map",
+    mandarin: "地图",
+    pinyin: "dìtú"
+  },
+  {
+    id: 15,
+    english: "passport",
+    mandarin: "护照",
+    pinyin: "hùzhào"
+  },
+  {
+    id: 16,
+    english: "luggage",
+    mandarin: "行李",
+    pinyin: "xínglǐ"
+  },
+  {
+    id: 17,
+    english: "early",
+    mandarin: "早了",
+    pinyin: "zǎole"
+  },
+  {
+    id: 18,
+    english: "late",
+    mandarin: "迟到了",
+    pinyin: "chídàole"
+  }
+]
+const LOCATIONS = [
+  {
+      id: 1,
+      english: "Australia",
+      mandarin: "澳洲",
+      pinyin: "Àodàlìyǎ"
+  },
+  {
+    id: 2,
+    english: "China",
+    mandarin: "中国",
+    pinyin: "Zhōngguó"
+  },
+  {
+    id: 3,
+    english: "US",
+    mandarin: "美国",
+    pinyin: "Měiguó"
+  },
+  {
+    id: 4,
+    english: "UK",
+    mandarin: "英国",
+    pinyin: "Yīngguó"
+  },
+  {
+    id: 5,
+    english: "Melbourne",
+    mandarin: "墨尔本",
+    pinyin: "Mò'ěrběn"
+  },
+  {
+    id: 6,
+    english: "Shanghai",
+    mandarin: "上海",
+    pinyin: "Shànghǎi"
+  },
+  {
+    id: 7,
+    english: "Beijing",
+    mandarin: "北京",
+    pinyin: "Běijīng"
+  },
+  {
+    id: 8,
+    english: "Hong Kong",
+    mandarin: "香港",
+    pinyin: "Xiānggǎng"
+  },
+  {
+    id: 9,
+    english: "London",
+    mandarin: "伦敦",
+    pinyin: "Lúndūn"
+  },
+  {
+    id: 10,
+    english: "Paris",
+    mandarin: "巴黎",
+    pinyin: "Bālí"
+  },
+  {
+    id: 11,
+    english: "New York",
+    mandarin: "纽约",
+    pinyin: "Niǔyuē"
+  },
+  {
+    id: 12,
+    english: "English",
+    mandarin: "英文",
+    pinyin: "Yīngguó"
+  },
+  {
+    id: 13,
+    english: "Chinese",
+    mandarin: "中文",
+    pinyin: "Zhōngguó"
+  },
+  {
+    id: 14,
+    english: "French",
+    mandarin: "法文",
+    pinyin: "Fǎwén"
+  },
+  {
+    id: 15,
+    english: "nationality",
+    mandarin: "国籍",
+    pinyin: "guójí"
+  },
+  {
+    id: 16,
+    english: "country",
+    mandarin: "国家",
+    pinyin: "guójiā"
+  },
+  {
+    id: 17,
+    english: "city",
+    mandarin: "城市",
+    pinyin: "chéngshì"
+  },
+  {
+    id: 18,
+    english: "state/province",
+    mandarin: "州",
+    pinyin: "zhōu"
   }
 ]
