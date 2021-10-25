d2fb16c57cb9d7cccb31869077a2314089595e11
scottwarburton <scott_warburton@hotmail.com>
2021-10-25 18:36:45 +1100

    Debug

--- server/server/settings.py
+++ server/server/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
